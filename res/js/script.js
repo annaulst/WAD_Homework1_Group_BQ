1ce6bd37901514e4700eeb8b1c9e965547e61a31
NormanPirk <norman.pirk@gmail.com>
2021-10-18 10:57:45 +0300

    retrieving posts.json both online and locally

--- res/js/script.js
+++ res/js/script.js
@@ -23,6 +23,7 @@ $(function(){
     let section = $('<section id="main">');
     let postsContainer = $('<div class="posts-container">');
 
+    // retrieve posts.json from online storage
     $.get("http://myjson.dit.upm.es/api/bins/1sbt", function(json_obj) {
         for (obj of json_obj) {
             console.log(obj);
@@ -51,6 +52,37 @@ $(function(){
             postsContainer.append(post);
         }
     });
+
+    // retrieve posts.json locally
+    /* $.get("res/json/posts.json", function(json_obj) {
+        for (obj of json_obj) {
+            console.log(obj);
+            
+            let post = $('<div class="post"></div>');
+            let postHeaderDiv = $('<div>');
+            let userImage = $('<img id="user-image" src="res/images/login.png"/>');
+            let date = $('<p>').text(obj.date);
+            let contentDiv = $('<div class="post-content">');
+            let postDescription = $('<p class="post-description">').text(obj.post);
+            let likeButton = $('<button class="btn btn-like">');
+            let i = $('<i class="fa fa-thumbs-up">');
+
+            likeButton.append(i);
+            postHeaderDiv.append(userImage, date);
+            contentDiv.append(postDescription, likeButton);
+            
+            if (obj.img == null) {
+                post.append(postHeaderDiv, contentDiv);
+            } else {
+                let postImg = $('<img>').attr("src", obj.img);
+                postImg.attr("alt", obj.imgDescription);
+                post.append(postHeaderDiv, postImg, contentDiv);
+            }
+
+            postsContainer.append(post);
+        }
+    }); */
+
 
     section.append(postsContainer);
     $('body').append(section);
