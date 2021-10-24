1c1b84bc96fcb3602351738f0ca1192006c6e9a6
NormanPirk <norman.pirk@gmail.com>
2021-10-24 21:05:03 +0300

    retrieve posts.json locally

--- res/js/script.js
+++ res/js/script.js
@@ -44,38 +44,7 @@ $(function(){
     // json validator: https://jsonlint.com/
 
     // retrieve posts.json from online storage
-    $.get("http://myjson.dit.upm.es/api/bins/7469", function(json_obj) {
-        for (obj of json_obj) {
-            console.log(obj);
-            
-            let post = $('<div class="post"></div>');
-            let postHeaderDiv = $('<div>');
-            let userImage = $('<img id="user-image"/>');
-            userImage.attr("src", obj.userImgSrc);
-            let date = $('<p>').text(obj.date);
-            let contentDiv = $('<div class="post-content">');
-            let postDescription = $('<p class="post-description">').text(obj.post);
-            let likeButton = $('<button class="btn btn-like">');
-            let i = $('<i class="fa fa-thumbs-up">');
-
-            likeButton.append(i);
-            postHeaderDiv.append(userImage, date);
-            contentDiv.append(postDescription, likeButton);
-            
-            if (obj.img == null) {
-                post.append(postHeaderDiv, contentDiv);
-            } else {
-                let postImg = $('<img>').attr("src", obj.img);
-                postImg.attr("alt", obj.imgDescription);
-                post.append(postHeaderDiv, postImg, contentDiv);
-            }
-
-            postsContainer.append(post);
-        }
-    });
-
-    // retrieve posts.json locally
-    /* $.get("res/json/posts.json", function(json_obj) {
+    /* $.get("http://myjson.dit.upm.es/api/bins/7469", function(json_obj) {
         for (obj of json_obj) {
             console.log(obj);
             
@@ -104,6 +73,37 @@ $(function(){
             postsContainer.append(post);
         }
     }); */
+
+    // retrieve posts.json locally
+    $.get("res/json/posts.json", function(json_obj) {
+        for (obj of json_obj) {
+            console.log(obj);
+            
+            let post = $('<div class="post"></div>');
+            let postHeaderDiv = $('<div>');
+            let userImage = $('<img id="user-image"/>');
+            userImage.attr("src", obj.userImgSrc);
+            let date = $('<p>').text(obj.date);
+            let contentDiv = $('<div class="post-content">');
+            let postDescription = $('<p class="post-description">').text(obj.post);
+            let likeButton = $('<button class="btn btn-like">');
+            let i = $('<i class="fa fa-thumbs-up">');
+
+            likeButton.append(i);
+            postHeaderDiv.append(userImage, date);
+            contentDiv.append(postDescription, likeButton);
+            
+            if (obj.img == null) {
+                post.append(postHeaderDiv, contentDiv);
+            } else {
+                let postImg = $('<img>').attr("src", obj.img);
+                postImg.attr("alt", obj.imgDescription);
+                post.append(postHeaderDiv, postImg, contentDiv);
+            }
+
+            postsContainer.append(post);
+        }
+    });
 
 
     section.append(postsContainer);
